4d0670967461996ba08cd5a689c29cacc3c6738d
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2022-08-01 11:27:31 -0500

    Update ob2.cpp

--- ob2.cpp
+++ ob2.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <list>
 #include <string>
+using namespace std;
 
 class IObserver {
  public:
   virtual ~IObserver(){};
-  virtual void Update(const std::string &message_from_subject) = 0;
+  virtual void Update(const string &message_from_subject) = 0;
 };
 
 class ISubject {
@@ -17,72 +18,64 @@ class ISubject {
 };
 
 class Subject : public ISubject {
- public:
-  virtual ~Subject() {
-    std::cout << "Goodbye, I was the Subject.\n";
-  }
-
-  void Attach(IObserver *observer) override {
-    list_observer_.push_back(observer);
-  }
-  void Detach(IObserver *observer) override {
-    list_observer_.remove(observer);
-  }
-  void Notify() override {
-    std::list<IObserver *>::iterator iterator = list_observer_.begin();
-    HowManyObserver();
-    while (iterator != list_observer_.end()) {
-      (*iterator)->Update(message_);
-      ++iterator;
+    private:
+        list<IObserver *> list_observer_;
+        string message_;
+    public:
+        virtual ~Subject() {
+            cout << "Goodbye, I was the Subject.\n";
+        }
+    void Attach(IObserver *observer) override {
+        list_observer_.push_back(observer);
     }
-  }
+    void Detach(IObserver *observer) override {
+        list_observer_.remove(observer);
+    }
+    void Notify() override {
+        list<IObserver *>::iterator iterator = list_observer_.begin();
+        HowManyObserver();
+        while (iterator != list_observer_.end()) {
+            (*iterator)->Update(message_);
+            ++iterator;
+        }
+    }
 
-  void CreateMessage(std::string message = "Empty") {
-    this->message_ = message;
-    Notify();
-  }
-  void HowManyObserver() {
-    std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
-  }
-  void SomeBusinessLogic() {
-    this->message_ = "change message message";
-    Notify();
-    std::cout << "I'm about to do some thing important\n";
-  }
-
- private:
-  std::list<IObserver *> list_observer_;
-  std::string message_;
+    void CreateMessage(string message = "Empty") {
+        this->message_ = message;
+        Notify();
+    }
+    void HowManyObserver() {
+        cout << "There are " << list_observer_.size() << " observers in the list.\n";
+    }
 };
 
 class Observer : public IObserver {
- public:
-  Observer(Subject &subject) : subject_(subject) {
-    this->subject_.Attach(this);
-    std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
-    this->number_ = Observer::static_number_;
-  }
-  virtual ~Observer() {
-    std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
-  }
+    private:
+        string message_from_subject_;
+        Subject &subject_;
+        static int static_number_;
+        int number_;
+    public:
+        Observer(Subject &subject) : subject_(subject) {
+            this->subject_.Attach(this);
+            cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
+            this->number_ = Observer::static_number_;
+        }
+        virtual ~Observer() {
+            cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
+        }
 
-  void Update(const std::string &message_from_subject) override {
-    message_from_subject_ = message_from_subject;
-    PrintInfo();
-  }
-  void RemoveMeFromTheList() {
-    subject_.Detach(this);
-    std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
-  }
-  void PrintInfo() {
-    std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
-  }
-
- private:
-  std::string message_from_subject_;
-  Subject &subject_;
-  static int static_number_;
-  int number_;
+        void Update(const string &message_from_subject) override {
+            message_from_subject_ = message_from_subject;
+            PrintInfo();
+        }
+        void RemoveMeFromTheList() {
+            subject_.Detach(this);
+            cout << "Observer \"" << number_ << "\" removed from the list.\n";
+        }
+        void PrintInfo() {
+            cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
+        }
 };
 
 int Observer::static_number_ = 0;
