7d22ea371779bbfccf86d559bbe3861a998142f8
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-08-01 09:10:08 -0500

    Create Command.cpp

---
+++ Command.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+ 
+using namespace std;
+
+// Command Interface
+class Command
+{
+public:
+	virtual void execute() = 0;
+};
+ 
+// Receiver Class
+class Light 
+{
+public:
+	void on() {
+		cout << "La luz esta encendida\n";
+	}
+	void off() {
+		cout << "La luz esta apagada\n";
+	}
+}; 
+
+// Command para encender la luz
+class LightOnCommand : public Command 
+{
+private:
+	Light *mLight;
+public:
+        LightOnCommand(Light *light) : mLight(light) {}
+	void execute(){
+		mLight->on();
+	}
+
+};
+ 
+// Command para apagar la luz
+class LightOffCommand : public Command 
+{
+private:
+	Light *mLight;
+public:
+        LightOffCommand(Light *light) : mLight(light) {}
+	void execute(){
+		mLight->off();
+	}
+
+};
+
+// Invoker 
+// Almacena el objeto comando
+class RemoteControl 
+{
+private:
+	Command *mCmd;
+public:
+	void setCommand(Command *cmd) {
+		mCmd = cmd;
+	}
+
+	void buttonPressed() {
+		mCmd->execute();
+	} 
+
+};
+ 
+// The client
+int main() 
+{
+	// Receiver 
+	Light *light = new Light;
+
+	// concrete Command objects 
+	LightOnCommand *lightOn = new LightOnCommand(light);
+	LightOffCommand *lightOff = new LightOffCommand(light);
+
+	// invoker objects
+	RemoteControl *control = new RemoteControl;
+
+	// execute
+	control->setCommand(lightOn);
+	control->buttonPressed();
+	control->setCommand(lightOff);
+	control->buttonPressed();
+
+	delete light, lightOn, lightOff, control;
+
+	return 0;
+}
